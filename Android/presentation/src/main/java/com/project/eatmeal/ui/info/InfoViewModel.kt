5f374e043e23a5cfc78170a7e94a6f057ad50ce2
GyuSeok Nam <namgyeseok0614@naver.com>
2021-06-11 09:44:32 +0900

    [UPDATE] Question Answer

--- Android/presentation/src/main/java/com/project/eatmeal/ui/info/InfoViewModel.kt
+++ Android/presentation/src/main/java/com/project/eatmeal/ui/info/InfoViewModel.kt
@@ -6,5 +6,25 @@ import com.project.eatmeal.base.BaseViewModel
 
 class InfoViewModel : BaseViewModel() {
 
+    val answer1 = MutableLiveData<Boolean>(false)
+    val answer2 = MutableLiveData<Boolean>(false)
+    val answer3 = MutableLiveData<Boolean>(false)
+    val answer4 = MutableLiveData<Boolean>(false)
+
+    fun clickQ1() {
+        answer1.value = !answer1.value!!
+    }
+
+    fun clickQ2() {
+        answer2.value = !answer2.value!!
+    }
+
+    fun clickQ3() {
+        answer3.value = !answer3.value!!
+    }
+
+    fun clickQ4() {
+        answer4.value = !answer4.value!!
+    }
 
 }
